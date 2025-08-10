dd28a20b673ee1bf68876bbb190159688d555051
qiushui7 <997179306@qq.com>
2025-08-10 14:10:46 +0800

    feat: footer
    Signed-off-by: qiushui7 <997179306@qq.com>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import ClientTurbulenceBackground from "./components/ClientTurbulenceBackground";
 
 const geistSans = Geist({
@@ -35,7 +36,10 @@ export default function RootLayout({
       >
         <ClientTurbulenceBackground />
         <Header />
-        {children}
+        <main className="min-h-screen">
+          {children}
+        </main>
+        <Footer />
       </body>
     </html>
   );
